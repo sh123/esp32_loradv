2d324cd0f5cb92bb10624f0e59a9e98eb0805fe4
sh123 <sh123@users.noreply.github.com>
2022-12-19 17:34:21 +0200

    Config update

--- include/loradv_service.h
+++ include/loradv_service.h
@@ -14,7 +14,14 @@
 #include <CircularBuffer.h>
 #include <arduino-timer.h>
 
+#if __has_include("/tmp/esp32_loradv_config.h")
+#pragma message("Using external config")
+#include "/tmp/esp32_loradv_config.h"
+#else
+#pragma message("Using default built-in config")
 #include "config.h"
+#endif
+
 #include "loradv_config.h"
 
 namespace LoraDv {
